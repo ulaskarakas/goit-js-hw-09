120c9b061b7abcee033c96480f7c1d2d733dabfd
ulaskarakas <ulaskarakas95@gmail.com>
2026-07-18 18:35:09 +0300

    Change something in vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import SortCss from 'postcss-sort-media-queries';
 export default defineConfig(({ command }) => {
   return {
     define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+      global: 'globalThis',
     },
     root: 'src',
     build: {
